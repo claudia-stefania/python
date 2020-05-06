15f9cd1e73ec4927e0b69403623ffae5c2867d7a
claudia-stefania <claudiasparloaga@gmail.com>
2020-05-06 10:18:06 +0300

    added class

--- PythonReleasesVersion.py
+++ PythonReleasesVersion.py
@@ -3,34 +3,44 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
-chromeDriver = webdriver.Chrome("C:/Users/cparloaga/Documents/chromedriverWin32/chromedriver")
-chromeDriver.get("https://www.python.org")
-print(chromeDriver.title)
 
-action = ActionChains(chromeDriver)
+class ChromeSearch:
 
-downloadsMenu = chromeDriver.find_element_by_id("downloads")
-action.move_to_element(downloadsMenu).perform()
+    def __init__(self):
+        self.chromeDriver = webdriver.Chrome("C:/Users/cparloaga/Documents/chromedriverWin32/chromedriver")
 
-allReleasesMenu = chromeDriver.find_element_by_xpath(".//li[@id='downloads']/ul[@class='subnav menu']/li[@class='tier-2 element-1']")
-action.move_to_element(allReleasesMenu).perform()
-allReleasesMenu.click()
+    def releases(self):
+        chromeDriver = self.chromeDriver
+        chromeDriver.get("https://www.python.org")
+        print(chromeDriver.title)
 
-print(chromeDriver.current_url)
+        action = ActionChains(chromeDriver)
 
-releasesSection = chromeDriver.find_element_by_xpath(".//div[@class='row active-release-list-widget']/ol[@class='list-row-container menu']")
-releasesVersions = releasesSection.find_elements_by_class_name("release-version")
+        downloadsMenu = chromeDriver.find_element_by_id("downloads")
+        action.move_to_element(downloadsMenu).perform()
 
-# versionsList = []
-# for item in releasesVersions:
-#     versionsList.append(item.text)
-#
-# versionsList.sort(reverse=True)
-# print("Latest version is: " + versionsList[0])
+        allReleasesMenu = chromeDriver.find_element_by_xpath(
+            ".//li[@id='downloads']/ul[@class='subnav menu']/li[@class='tier-2 element-1']")
+        action.move_to_element(allReleasesMenu).perform()
+        allReleasesMenu.click()
 
-# for item in releasesVersions:
-#     print(item.text)
-print("Latest version is: " + releasesVersions[0].text)
+        print(chromeDriver.current_url)
 
-sleep(5)
-chromeDriver.close()
+        releasesSection = chromeDriver.find_element_by_xpath(
+            ".//div[@class='row active-release-list-widget']/ol[@class='list-row-container menu']")
+        releasesVersions = releasesSection.find_elements_by_class_name("release-version")
+
+        # versionsList = []
+        # for item in releasesVersions:
+        #     versionsList.append(item.text)
+        #
+        # versionsList.sort(reverse=True)
+        # print("Latest version is: " + versionsList[0])
+
+        # for item in releasesVersions:
+        #     print(item.text)
+        print("Latest version is: " + releasesVersions[0].text)
+
+    def tearDown(self):
+        sleep(5)
+        self.chromeDriver.close()
