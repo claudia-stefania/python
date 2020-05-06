517a8c7c6b6c6f28007d3211d9bb3bbed8080c1f
claudia-stefania <claudiasparloaga@gmail.com>
2020-05-06 10:45:26 +0300

    update

--- Jokes.py
+++ Jokes.py
@@ -4,17 +4,16 @@ ROOT_END_POINT = 'https://official-joke-api.appspot.com/'
 
 
 class Joke:
-    path = ''
 
     def __init__(self, path):
         self.path = ROOT_END_POINT + path
 
-    def joke(self):
+    def request(self):
         response = requests.get(self.path)
         return response
 
     def jokes(self):
-        jokesDictionary = self.joke().json()
+        jokesDictionary = self.request().json()
         for joke in jokesDictionary:
             print("They say : " + joke.get('setup'))
             print("And the joke is: " + joke.get('punchline'))
